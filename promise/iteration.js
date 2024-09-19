2663c2d84cc1cde8c8a87ce21d30a4b1b7af7ab2
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-09-19 09:38:11 +0530

    changed js

---
+++ promise/iteration.js
@@ -0,0 +1,120 @@
+// // for (let i = 0; i <= 10; i++) {
+// //     const element = i;
+// //     if (element == 5) {
+// //         console.log("5 is best number");
+// //     }
+// //     console.log(element);
+    
+// // }
+
+// const array=[1,2,3,4,5,6,7,8]
+
+// // for (let index = 0; index < array.length; index++) {
+// //     const element = array[index];
+// //     console.log(element);
+    
+// // }
+
+
+// // for (let index = 0; index < array.length; index++) {
+// //     console.log(array[index]);
+    
+// // }
+
+// // for (let index = 0; index < 10; index++) {
+// //    console.log(` value of i is ${index}`);
+// //     for (let j = 0; j<= index; j++) {
+// //         console.log(` value of j is ${j}`);
+        
+// //     }
+
+// // }
+// // 
+
+
+// // for (let index = 1; index <= 20; index++) {
+// //         if (index == 5) {
+// //             console.log(`Detected 5`);
+// //             continue
+// //         }
+// //        console.log(`Value of i is ${index}`);
+        
+// //     }
+
+
+
+
+
+// //     const string="mimohshuklaa"
+// //     for (const iterator of string) {
+// //         console.log(iterator);
+        
+// //     }
+// // 
+
+//     // const array2=["mimohshukla","iimt"]
+//     // for (const mimo of array2) {
+//     //    console.log(mimo);
+//     // }
+
+//     const object1={
+//         name:"mimohshukla",
+//         id:72330919999
+//     }
+//     // for (const shukla of object) {
+//     //     console.log(shukla);
+//     // }
+//     // 
+   
+
+
+//     // const map = new Map();
+//     // map.set('IN', "India")
+//     // map.set('USA', "United States of America")
+//     // map.set('Fr', "France")
+//     // map.set('IN', "India")
+
+
+//     // console.log(map);
+
+
+//     // for (const [key,value] of map) {
+//     //    console.log(`key is ${key} and value is ${value}`);
+//     // }
+
+// const programming = ["js", "rb", "py", "java", "cpp"]
+
+// for (const key in programming) {
+//     console.log(programming[key]);
+//     console.log(key);
+// }
+
+// const object2={
+//     name:"mimohshukla",
+//     class:"section-c"
+// }
+
+// for (const key in object2) {
+//     console.log(key);
+//     console.log(`${key} shortcut is for ${object2[key]}`);
+
+
+
+   
+// }
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);}
+
+
+
+const array=[1,2,3,4,56,7,8,88,9]
+array.forEach(element => {
+   console.log(element);
+});
